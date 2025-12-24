043862ece0a1b47b3463457a05353d22ff7e82e1
sayan9r <sayan18082005@gmail.com>
2025-12-24 21:59:05 +0530

    fixing the api endpoint

--- src/api.js
+++ src/api.js
@@ -1,23 +1,25 @@
 // Base API URL (read from Vite env). Use `VITE_API_URL` in your `.env` files.
-export const API_URL = import.meta.env.VITE_API_URL || import.meta.env.API_URL ;
+export const API_URL = import.meta.env.VITE_API_URL || import.meta.env.API_URL;
 
-//Auth path segments and endpoints
-export const AUTH_BASE = '/api/auth';
+// Auth path segments
+export const AUTH_BASE = "/api/auth";
+export const AUTH_USER = "/api/users";
+
+// Auth Endpoints
 export const AUTH_LOGIN = `${AUTH_BASE}/login`;
 export const AUTH_LOGOUT = `${AUTH_BASE}/logout`;
 export const AUTH_REGISTER = `${AUTH_BASE}/register`;
-export const AUTH_CONTACTUS = `${AUTH_BASE}/contactus`;
-export const AUTH_CREATEGAME = `${AUTH_BASE}/creategame`;
-export const AUTH_JOINGAME = `${AUTH_BASE}/joingame`;
-export const AUTH_ALLCREATEGAME = `${AUTH_BASE}/allcreategame`;
-export const AUTH_MYJOINEDGAMES = `${AUTH_BASE}/myjoinedgames`;
-export const AUTH_JOINREQUEST = `${AUTH_BASE}/joinrequest`;
-export const AUTH_JOINSTATUS = `${AUTH_BASE}/joinstatus`;
-export const AUTH_LASTCREATEGAME = `${AUTH_BASE}/lastcreategame`;
-export const AUTH_STARTGAME = `${AUTH_BASE}/startgame`;
-export const AUTH_GAME_REQUESTS = `${AUTH_BASE}/game-requests`;
-export const AUTH_TOTAL_ACCEPTED = `${AUTH_BASE}/total-accepted`;
-export const AUTH_UPDATE_REQUESTS = `${AUTH_BASE}/update-requests`;
 
-//  export const AUTH_LOGIN = `/login`;
-//  export const AUTH_REGISTER = `/register`;
+// User Endpoints
+export const AUTH_CONTACTUS = `${AUTH_USER}/contactus`;
+export const AUTH_CREATEGAME = `${AUTH_USER}/creategame`;
+export const AUTH_JOINGAME = `${AUTH_USER}/joingame`;
+export const AUTH_ALLCREATEGAME = `${AUTH_USER}/allcreategame`;
+export const AUTH_MYJOINEDGAMES = `${AUTH_USER}/myjoinedgames`;
+export const AUTH_JOINREQUEST = `${AUTH_USER}/joinrequest`;
+export const AUTH_JOINSTATUS = `${AUTH_USER}/joinstatus`;
+export const AUTH_LASTCREATEGAME = `${AUTH_USER}/lastcreategame`;
+export const AUTH_STARTGAME = `${AUTH_USER}/startgame`;
+export const AUTH_GAME_REQUESTS = `${AUTH_USER}/game-requests`;
+export const AUTH_TOTAL_ACCEPTED = `${AUTH_USER}/total-accepted`;
+export const AUTH_UPDATE_REQUESTS = `${AUTH_USER}/update-requests`;
