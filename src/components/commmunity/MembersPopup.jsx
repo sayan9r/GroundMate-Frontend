d364d1cf276635dc4ab720b804ca4013a3ce1930
sayan9r <sayan18082005@gmail.com>
2026-01-15 20:57:09 +0530

    removing the cancel button

--- src/components/commmunity/MembersPopup.jsx
+++ src/components/commmunity/MembersPopup.jsx
@@ -71,15 +71,7 @@ const MembersPopup = ({ isOpen, onClose, communityId }) => {
           )}
         </div>
 
-        {/* Footer */}
-        <div className="mt-4 text-right">
-          <button
-            onClick={onClose}
-            className="px-4 py-2 bg-red-600 hover:bg-red-700 rounded"
-          >
-            Cancel
-          </button>
-        </div>
+        
       </div>
     </div>
   );
